fdecf391a86794a935b2cbf99404ed02c174557a
Leandro Moreira <lendrodev.oliveira@gmail.com>
2020-08-27 20:56:53 -0300

    Criação do pop up

---
+++ popup-script.js
@@ -0,0 +1,22 @@
+ 
+const popUp = ()=> {
+    const modal = document.querySelector('#modal-pop-up')
+    modal.classList.remove('container-pop-up')
+    modal.classList.add('container-pop-up-active')
+}
+
+/*Botão que fecha o pop-up*/
+const closePopUp = ()=>{
+    const btnClose = document.querySelector('.close')
+    btnClose.addEventListener('click',()=>{
+        const modal = document.querySelector('#modal-pop-up')
+        modal.classList.add('container-pop-up')
+        modal.classList.remove('container-pop-up-active')
+    } )
+}
+
+const modalPopUp = document.querySelector('body')
+modalPopUp.addEventListener('mouseleave',()=>{
+    popUp()
+    closePopUp()
+})
